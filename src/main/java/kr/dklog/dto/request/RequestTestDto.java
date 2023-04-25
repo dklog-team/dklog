141292099307a883d8e327e58044fdc43a9e9ffe
cheoljin408 <kcj409@naver.com>
2023-04-26 00:43:35 +0900

    feat: header fragment 구현 완료
    - footer fragment 구현하고 layout에 header 추가
    - dto 패키지에 request, response 패키지 추가
    
    #4

--- src/main/java/kr/dklog/dto/request/RequestTestDto.java
+++ src/main/java/kr/dklog/dto/request/RequestTestDto.java
@@ -1,2 +1,19 @@
-package kr.dklog.dto.request;public class RequestTestDto {
+package kr.dklog.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestTestDto {
+
+    private Long id;
+
+    private String testColumn1;
+
+    private String testColumn2;
 }
--- src/main/java/kr/dklog/dto/response/ResponseTestDto.java
+++ src/main/java/kr/dklog/dto/response/ResponseTestDto.java
@@ -1,2 +1,19 @@
-package kr.dklog.dto.response;public class ResponseTestDto {
+package kr.dklog.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTestDto {
+
+    private Long id;
+
+    private String testColumn1;
+
+    private String testColumn2;
 }
