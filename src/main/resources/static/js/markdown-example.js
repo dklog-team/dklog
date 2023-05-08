d9385313d84d298622770b87c7b67035db07348d
sylee990205 <sylee.990205@gmail.com>
2023-05-08 11:50:39 +0900

    feat: 튜토리얼 페이지 추가 완료
    - 튜토리얼 페이지 추가
    
    #85

---
+++ src/main/resources/static/js/markdown-example.js
@@ -0,0 +1,104 @@
+const markdownExample = '# h1 Heading :wink:\n' +
+    '## h2 Heading\n' +
+    '### h3 Heading\n' +
+    '#### h4 Heading\n' +
+    '##### h5 Heading\n' +
+    '###### h6 Heading\n' +
+    '\n' +
+    '\n' +
+    '## 수평선\n' +
+    '\n' +
+    '___\n' +
+    '\n' +
+    '---\n' +
+    '\n' +
+    '***\n' +
+    '\n' +
+    '## Typographic 대체\n' +
+    '\n' +
+    '(c) (C) (r) (R) (tm) (TM) (p) (P) +-\n' +
+    '\n' +
+    'test.. test... test..... test?..... test!....\n' +
+    '\n' +
+    '!!!!!! ???? ,,  -- ---\n' +
+    '\n' +
+    '## 강조\n' +
+    '\n' +
+    '**두꺼운 텍스트**\n' +
+    '\n' +
+    '__두꺼운 텍스트__\n' +
+    '\n' +
+    '*기울어진 텍스트*\n' +
+    '\n' +
+    '_기울어진 텍스트_\n' +
+    '\n' +
+    '~~취소된 텍스트~~\n' +
+    '\n' +
+    '==마크된 텍스트==\n' +
+    '\n' +
+    '## 인용\n' +
+    '\n' +
+    '> ">"으로 시작하는 텍스트\n' +
+    '>> ">"는 3개까지 가능합니다.\n' +
+    '>>> 마지막 인용구\n' +
+    '\n' +
+    '\n' +
+    '## 리스트\n' +
+    '\n' +
+    '순서가 없는 리스트\n' +
+    '\n' +
+    '+ `+`, `-`, `*` 가능\n' +
+    '+ 오늘의 메뉴\n' +
+    '  - 중식\n' +
+    '    * 짜장면\n' +
+    '    + 짬뽕\n' +
+    '    - 탕수육\n' +
+    '\n' +
+    '순서가 있는 리스트\n' +
+    '\n' +
+    '1. 아침\n' +
+    '2. 점심\n' +
+    '1. 저녁\n' +
+    '\n' +
+    '순서 초기화:\n' +
+    '\n' +
+    '57. 아침\n' +
+    '58. 점심\n' +
+    '1. 저녁\n' +
+    '\n' +
+    '\n' +
+    '## Code\n' +
+    '\n' +
+    'Inline `code`\n' +
+    '\n' +
+    'Block code "fences"\n' +
+    '\n' +
+    '```\n' +
+    'Sample text here...\n' +
+    '```\n' +
+    '\n' +
+    'Syntax highlighting\n' +
+    '\n' +
+    '``` java\n' +
+    'pubic static void main(String[] args){\n' +
+    '  System.out.println("hello java");\n' +
+    '}\n' +
+    '```\n' +
+    '\n' +
+    '## 테이블\n' +
+    '\n' +
+    '| 이름 | 생년월일 | 주소 |\n' +
+    '| ------ | ----------- | ------ |\n' +
+    '| 홍길동 | 680126 | 서울특별시 강남구 대치동 |\n' +
+    '| 김철수 | 021113 | 제주특별자치도 제주시 애월읍 | \n' +
+    '\n' +
+    '## 링크\n' +
+    '\n' +
+    '[dklog](https://github.com/dklog-team)\n' +
+    '\n' +
+    'dklog team link https://github.com/dklog-team (enable linkify to see)\n' +
+    '\n' +
+    '\n' +
+    '## 이미지\n' +
+    '\n' +
+    '![dklog](http://39.118.47.163:9094/dklog-organization.jpeg)';
