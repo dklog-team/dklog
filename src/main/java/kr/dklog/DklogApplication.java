94b8ecf403b5fadbbf784071d6163912e295a1d2
cheoljin408 <kcj409@naver.com>
2023-05-24 18:55:59 +0900

    feat: 사이트 방문자 기록
    close #105

--- src/main/java/kr/dklog/DklogApplication.java
+++ src/main/java/kr/dklog/DklogApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
-@Getter
 public class DklogApplication {
 
     public static void main(String[] args) {
